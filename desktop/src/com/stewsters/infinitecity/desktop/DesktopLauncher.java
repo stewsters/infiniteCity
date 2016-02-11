776376abd9e2415a387d7b3544d0a8ca4eae658b
Adrian Moore <stewsters@gmail.com>
2016-02-11 14:44:34 -0600

    initial commit

---
+++ desktop/src/com/stewsters/infinitecity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stewsters.infinitecity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stewsters.infinitecity.CityGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CityGame(), config);
+	}
+}
