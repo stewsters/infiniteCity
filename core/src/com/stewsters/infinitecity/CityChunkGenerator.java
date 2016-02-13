c0c024488f13e794794b4a2accd243f0a97713f6
Adrian Moore <stewsters@gmail.com>
2016-02-13 11:48:55 -0600

    Small city generation, next is time to add variety

---
+++ core/src/com/stewsters/infinitecity/CityChunkGenerator.java
@@ -0,0 +1,151 @@
+package com.stewsters.infinitecity;
+
+import com.stewsters.util.math.MatUtils;
+import com.stewsters.util.math.geom.Rect;
+import com.stewsters.util.math.geom.RectPrism;
+import com.stewsters.util.math.geom.RectSubdivider;
+
+import java.util.List;
+
+/**
+ * Adapted from <a href="http://devmag.org.za/2009/04/25/perlin-noise/">http://devmag.org.za/2009/04/25/perlin-noise/</a>
+ *
+ * @author badlogic
+ */
+public class CityChunkGenerator {
+
+
+    public static final int groundHeight = 4;
+
+    private int xSize;
+    private int ySize;
+    private int zSize;
+    private byte[][][] tiles = null;
+
+    public CityChunkGenerator(int xSize, int ySize, int zSize) {
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.zSize = zSize;
+        this.tiles = new byte[xSize][ySize][zSize];
+    }
+
+
+    public static void generateVoxels(VoxelWorld voxelWorld) {
+
+
+        for (int xg = 0; xg < voxelWorld.chunksX; xg++) {
+            for (int yg = 0; yg < voxelWorld.chunksY; yg++) {
+                for (int zg = 0; zg < voxelWorld.chunksZ; zg++) {
+
+                    CityChunkGenerator cityChunkGenerator = new CityChunkGenerator(voxelWorld.CHUNK_SIZE_X, voxelWorld.CHUNK_SIZE_Y, voxelWorld.CHUNK_SIZE_Z);
+
+                    cityChunkGenerator.flattenWorld();
+                    cityChunkGenerator.constructBuildings(8);
+
+                    for (int x = 0; x < voxelWorld.CHUNK_SIZE_X; x++) {
+                        for (int y = 0; y < voxelWorld.CHUNK_SIZE_Y; y++) {
+                            for (int z = 0; z < voxelWorld.CHUNK_SIZE_Z; z++) {
+                                voxelWorld.set(
+                                        xg * VoxelWorld.CHUNK_SIZE_X + x,
+                                        yg * VoxelWorld.CHUNK_SIZE_Y + y,
+                                        zg * VoxelWorld.CHUNK_SIZE_Z + z,
+                                        cityChunkGenerator.tiles[x][y][z]);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+    }
+
+    private void flattenWorld() {
+        for (int x = 0; x < xSize; x++) {
+            for (int y = 0; y < ySize; y++) {
+                for (int z = 0; z < zSize; z++) {
+
+                    tiles[x][y][z] = z <= groundHeight ? (byte) 1 : (byte) 0;
+                }
+            }
+        }
+    }
+
+
+    public void constructBuildings(int minSize) {
+
+        //check average ySize for this
+
+        // make a lot representing the whole, except for the street
+        Rect whole = new Rect(2, 2, xSize - 3, ySize - 3);
+
+
+        List<Rect> lots = RectSubdivider.divide(whole, minSize);
+
+        for (Rect lot : lots) {
+            // ySize in floors
+            int totalFloors = MatUtils.getIntInRange(1, 8);
+
+            // This gives you the separation level around
+            int extendedWalk = MatUtils.getIntInRange(0, 2);
+
+            Rect foundation = new Rect(
+                    lot.x1 + extendedWalk,
+                    lot.y1 + extendedWalk,
+                    lot.x2 - extendedWalk,
+                    lot.y2 - extendedWalk);
+
+            for (int floor = 0; floor < totalFloors; floor++) {
+
+                wall(foundation, groundHeight + (floor * 3) + 1, 1, (byte) 1);
+//                wall(foundation, groundHeight + (floor * 3) + 2, (byte) 1);
+                solidLevel(foundation, groundHeight + (floor * 3) + 3, (byte) 1);
+            }
+
+        }
+
+        // make foundations
+
+    }
+
+    private void fillBlock(RectPrism prism, byte tileType) {
+
+        for (int x = prism.x1; x <= prism.x2; x++) {
+            for (int y = prism.y1; y <= prism.y2; y++) {
+                for (int z = prism.z1; z <= prism.z2; z++) {
+
+                    tiles[x][y][z] = tileType;
+                }
+            }
+        }
+    }
+
+
+    private void solidLevel(Rect prism, int z, byte tileType) {
+
+        for (int x = prism.x1; x <= prism.x2; x++) {
+            for (int y = prism.y1; y <= prism.y2; y++) {
+
+                tiles[x][y][z] = tileType;
+            }
+        }
+    }
+
+    // Draws a wall around the area
+    private void wall(Rect prism, int z, int wallHeight, byte tileType) {
+
+        for (int x = prism.x1; x <= prism.x2; x++) {
+            for (int y = prism.y1; y <= prism.y2; y++) {
+                for (int zp = 0; zp < wallHeight; zp++) {
+
+                    if ((x == prism.x1 || x == prism.x2) ||
+                            (y == prism.y1 || y == prism.y2))
+                        tiles[x][y][z + zp] = tileType;
+                }
+            }
+        }
+    }
+
+
+}
--- core/src/com/stewsters/infinitecity/CityGame.java
+++ core/src/com/stewsters/infinitecity/CityGame.java
@@ -3,25 +3,83 @@ package com.stewsters.infinitecity;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.math.MathUtils;
 
 public class CityGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch spriteBatch;
+    BitmapFont font;
+    ModelBatch modelBatch;
+    PerspectiveCamera camera;
+    Environment lights;
+    FirstPersonCameraController controller;
+    VoxelWorld voxelWorld;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        spriteBatch = new SpriteBatch();
+        font = new BitmapFont();
+        modelBatch = new ModelBatch();
+        DefaultShader.defaultCullFace = GL20.GL_FRONT;
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.near = 0.5f;
+        camera.far = 1000;
+
+        camera.direction.set(0, 1, 0);
+        camera.up.set(0, 0, 1);
+        controller = new FirstPersonCameraController(camera);
+        controller.setVelocity(50);
+
+        Gdx.input.setInputProcessor(controller);
+
+        lights = new Environment();
+        lights.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1.f));
+        lights.add(new DirectionalLight().set(1, 1, 1, 0.5f, 0.3f, 1));
+
+        Texture texture = new Texture(Gdx.files.internal("data/tiles.png"));
+        TextureRegion[][] tiles = TextureRegion.split(texture, 32, 32);
+
+        MathUtils.random.setSeed(0);
+        voxelWorld = new VoxelWorld(tiles[0], 5, 5, 1);
+        CityChunkGenerator.generateVoxels(voxelWorld);
+
+        float camX = voxelWorld.voxelsX / 2f;
+        float camY = voxelWorld.voxelsY / 2f; //getHighest(camX, camZ) + 1.5f;
+        float camZ = voxelWorld.voxelsZ / 2f;
+        camera.position.set(camX, camY, camZ);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        modelBatch.begin(camera);
+
+        modelBatch.render(voxelWorld, lights);
+        modelBatch.end();
+        controller.update();
+
+        spriteBatch.begin();
+        font.draw(spriteBatch, "fps: " + Gdx.graphics.getFramesPerSecond() + ", #visible chunks: " + voxelWorld.renderedChunks
+                + "/" + voxelWorld.numChunks, 0, 20);
+        spriteBatch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
+    }
 }
